42667381fcc26eac969cb4e29cf06231df3f7f36
OliverFjordside <insolfj@insatech.com>
2022-08-01 15:08:45 +0200

    All sorts of stuff

---
+++ Data/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace MudBlazorApp2.Data
+{
+	public class Device
+	{
+		public int Id { get; set; }
+		public string DeviceName { get; set; }
+		
+		public DateTime FetchedTime { get; set; }
+
+		public DateTime LastUpdated { get; set; }
+
+		public List<int> SensorValues { get; set; }
+
+	}
+}
